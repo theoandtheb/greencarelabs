12e0f2b4567b6933a88186c973dc5e399049755a
theoandtheb <blue.apple@sympatico.ca>
2017-03-04 15:49:40 -0500

    changed the fitbit webhook listener from a post to a get.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root :to => 'home#index'
   post "devices/accel" => "devices#accel"
   post "devices/fitbit" => "devices#fitbit"
-  post "devices/heyfromfitbit" => "devices#heyfromfitbit"
+  get "devices/heyfromfitbit" => "devices#heyfromfitbit"
 end
